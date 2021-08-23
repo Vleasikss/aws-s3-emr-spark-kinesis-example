a671d47fa326233e953c78901a110fb6bb93cab2
Vlas Bieliaiev <vlas.bieliaiev@nixsolutions.com>
2021-08-23 18:27:51 +0300

    ADDED WarehouseType documentation

--- cluster/src/main/scala/org/example/WarehouseType.java
+++ cluster/src/main/scala/org/example/WarehouseType.java
@@ -1,10 +1,46 @@
 package org.example;
 
 public enum WarehouseType {
+
     /**
-     * The smallest&cheapest Snowflake Warehouse instance
+     * Cost: 1 credit per hour
      */
-    X_SMALL("x-small");
+    X_SMALL("x-small"),
+
+    /**
+     * Cost: 2 credits per hour
+     */
+    SMALL("small"),
+
+    /**
+     * Cost: 4 credits per hour
+     */
+    MEDIUM("medium"),
+
+    /**
+     * Cost: 8 credits per hour
+     */
+    LARGE("large"),
+
+    /**
+     * Cost: 16 credits per hour
+     */
+    X_LARGE("x-large"),
+
+    /**
+     * Cost: 32 credits per hour
+     */
+    X2_LARGE("2x-large"),
+
+    /**
+     * Cost: 64 credits per hour
+     */
+    X3_LARGE("3x-large"),
+
+    /**
+     * Cost: 128 credits per hour
+     */
+    X4_LARGE("4x-large");
 
     private final String value;
 
